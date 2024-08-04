8dc2d1307f8ce1994f7e44b2f0a9ffdec836f61b
digvijaysingh21 <singh.v.digvijay@gmail.com>
2024-08-04 17:12:56 +0530

    build Vendor serializers, Views and urls

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from . import serializers
+from . import models
+
+
+class VendorList(generics.ListAPIView):
+    queryset= models.Vendor.objects.all()
+    serializer_class = serializers.VendorSerializer
+
